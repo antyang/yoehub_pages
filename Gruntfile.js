cd2c2a1dd96a77234198ca5ac50996f332bb419b
antyang <mrant0ny@gmail.com>
2015-09-12 23:31:21 -0700

    grunt-build-control

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,6 +9,9 @@
 
 module.exports = function (grunt) {
 
+  // Project configuration.
+  var pkg = require('./package.json');
+
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
 
@@ -438,7 +441,40 @@ module.exports = function (grunt) {
         configFile: 'test/karma.conf.js',
         singleRun: true
       }
+    },
+
+    // ---
+
+    // Various Grunt tasks...
+
+  buildcontrol: {
+    options: {
+      dir: 'dist',
+      commit: true,
+      push: true,
+      message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+    },
+    pages: {
+      options: {
+        remote: 'git@github.com:antyang/yoehub_pages.git',
+        branch: 'gh-pages'
+      }
+    },
+    heroku: {
+      options: {
+        remote: 'git@heroku.com:example-heroku-webapp-1988.git',
+        branch: 'master',
+        tag: pkg.version
+      }
+    },
+    local: {
+      options: {
+        remote: '../',
+        branch: 'build'
+      }
     }
+  }
+
   });
 
 
